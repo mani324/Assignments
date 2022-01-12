3e9dae4bfb1d9fb7fea711c3018e20204ff2eb0e
Manikandan V <95741161+mani324@users.noreply.github.com>
2022-01-12 18:47:25 +0530

    Add files via upload

---
+++ Assingment(12-01-2022)/Temperature.java
@@ -0,0 +1,9 @@
+class Temperature{
+	public static void main(String args[]){
+		int fahrenhiet=212;
+		int celsius=(fahrenhiet-32)*5/9;
+		System.out.println("Fahrenhiet to celsius is:"+celsius);
+	}
+}
+
+		
