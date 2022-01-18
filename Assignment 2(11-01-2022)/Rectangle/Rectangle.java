f14b9bffb8fb7c452eadf041d0211bce8244b511
Manikandan V <95741161+mani324@users.noreply.github.com>
2022-01-18 22:29:10 +0530

    Add files via upload

---
+++ Assignment 2(11-01-2022)/Rectangle/Rectangle.java
@@ -0,0 +1,10 @@
+public class Rectangle{
+	public static void main(String args[]){
+		int length=8,width=4;
+		int perimeter=2*(length+width),area=length*width;
+		System.out.println("Perimeter of the Rectangle is:"+perimeter);
+		System.out.println("Area of the Rectangle is:"+area);
+	}
+}
+
+		
