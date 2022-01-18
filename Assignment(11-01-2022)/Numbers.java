5e2562a5b0304664e71c1cb12480235d5aa0fd7c
Manikandan V <95741161+mani324@users.noreply.github.com>
2022-01-18 22:26:44 +0530

    Delete Assignment(11-01-2022) directory

--- Assignment(11-01-2022)/Numbers.java
+++
@@ -1,8 +0,0 @@
-public class Numbers{
-	public static void main(String args[]){
-		int num=8,square=num*num,cube=num*num*num,fourthPower=num*num*num*num;
-		System.out.println("Square of the number is:"+square);
-		System.out.println("Cube of the number is:"+cube);
-		System.out.println("Fourth power of number:"+fourthPower);
-	}
-}
