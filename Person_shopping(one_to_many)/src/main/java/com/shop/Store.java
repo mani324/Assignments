749f908b6af2f2abc73643d9257ef35bcb5ad4ea
Manikandan V <95741161+mani324@users.noreply.github.com>
2022-03-22 10:49:43 +0530

    Update Store.java

--- Person_shopping(one_to_many)/src/main/java/com/shop/Store.java
+++ Person_shopping(one_to_many)/src/main/java/com/shop/Store.java
@@ -21,23 +21,23 @@ public class Store {
     	  Person p=new Person();
     	  p.setName("Kabilan");
     	  Shopping shop1=new Shopping();
-    	  shop1.setShop_name("VM Supermarket");
-    	  shop1.setShop_location("Chennai Tamilnadu");
+    	  shop1.setShop_Name("VM Supermarket");
+    	  shop1.setShop_Location("Chennai Tamilnadu");
     	  shop1.setItem("Groceries");
     	  
     	  Shopping shop2=new Shopping();
-    	  shop2.setShop_name("VM Textile");
-    	  shop2.setShop_location("Chennai Tamilnadu");
+    	  shop2.setShop_Name("VM Textile");
+    	  shop2.setShop_Location("Chennai Tamilnadu");
     	  shop2.setItem("shirt");
     	  
     	  Shopping shop3=new Shopping();
-    	  shop3.setShop_name("VM TechMart");
-    	  shop3.setShop_location("Chennai Tamilnadu");
+    	  shop3.setShop_Name("VM TechMart");
+    	  shop3.setShop_Location("Chennai Tamilnadu");
     	  shop3.setItem("Laptop");
     	  
     	  Shopping shop4=new Shopping();
-    	  shop4.setShop_name("VM Foods");
-    	  shop4.setShop_location("Chennai Tamilnadu");
+    	  shop4.setShop_Name("VM Foods");
+    	  shop4.setShop_Location("Chennai Tamilnadu");
     	  shop4.setItem("Meals Parsel");
     	  ArrayList<Shopping>sp=new ArrayList<>();
     	  sp.add(shop1);
